bd66db30cae0ac70d8d0432f2a8848cadb86a22b
Mahsa Javadi <mahsa.javady@gmail.com>
2024-12-16 15:46:34 -0500

    added tic-tac-toe project

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TicTacToe from './components/tictactoe/TicTacToe';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <TicTacToe />
+      </header>
+    </div>
+  );
+}
+
+export default App;
